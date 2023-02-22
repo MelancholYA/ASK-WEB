bc246c4a91b44098e6a66b1d7bb483c94c850c07
yacine ouardi <ouardi.yacin3@gmail.com>
2023-02-22 13:48:03 +0100

    cd: start from scratch using create vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { SnackbarProvider } from "notistack";
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { UserContextProvider } from "./context/userContext";
-import ProtectForUsers from "./ProtectedRoutes/ProtectForUsers";
-import Auth from "./views/Auth";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-const App = () => {
-  const queryClient = new QueryClient();
-
-  const router = createBrowserRouter([
-    {
-      path: "/auth",
-      element: <Auth />,
-    },
-    {
-      path: "/dash",
-      element: <ProtectForUsers Element={() => <h1>Dash</h1>} />,
-    },
-  ]);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <SnackbarProvider>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <UserContextProvider>
-          <RouterProvider router={router} />
-        </UserContextProvider>
-      </QueryClientProvider>
-    </SnackbarProvider>
-  );
-};
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
 
-export { App };
+export default App
