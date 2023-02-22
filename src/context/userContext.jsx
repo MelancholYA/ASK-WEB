55f1282b3c911cd3bbe254181ab05c5cae5d2761
yacine ouardi <ouardi.yacin3@gmail.com>
2023-02-22 10:31:17 +0100

    fix: initiate user context state
    instead of initiating using useEffect i added it to the state on init

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,13 +3,11 @@ import { useStorage } from "../hooks/useStorage";
 
 const UserContext = createContext();
 const UserContextProvider = ({ children }) => {
-  const [userData, setUserData] = React.useState(null);
   const { getStorageData } = useStorage();
+  const [userData, setUserData] = React.useState(
+    getStorageData({ key: "askUserData", isObj: true })
+  );
 
-  useEffect(() => {
-    const user = getStorageData({ key: "askUserData", isObj: true });
-    setUserData(user);
-  }, []);
   return (
     <UserContext.Provider value={{ userData, setUserData }}>
       {children}
